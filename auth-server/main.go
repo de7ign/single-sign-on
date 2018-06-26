635831f33303a8430b2b9a8fadb0a720aaaef8b7
Nihal Murmu <nhlmrm@gmail.com>
2018-06-26 23:40:22 +0530

    Add google redirect auth URI

--- auth-server/main.go
+++ auth-server/main.go
@@ -4,14 +4,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
 
-type keys struct {
+type key struct {
 	Gg web `json:"gg"`
 	Fb web `json:"fb"`
 }
-
 
 type web struct {
 	ClientID                string   `json:"client_id"`
@@ -24,9 +24,25 @@ type web struct {
 	JavascriptOrigins       []string `json:"javascript_origins"`
 }
 
+func googleHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+	https://accounts.google.com/o/oauth2/v2/auth?scope=profile&access_type=offline&include_granted_scopes=true&
+	state=state_parameter_passthrough_value&redirect_uri=${ protocol }%3A%2F%2F${ encodeURIComponent(req.get('host')) }%
+	2Foauth2callback&response_type=code&client_id=303128741136-r2284o9d45b5c1lhpnhe3uvrd5df9def.apps.googleusercontent.com`)*/
+
+	http.Redirect(w, r, keys.Gg.AuthURI + "?scope=profile&access_type=offline" + 
+		"&include_granted_scopes=true&state=state_parameter_passthrough_value" +
+		"&redirect_uri=" + keys.Gg.RedirectURIs[1] + "&res&response_type=code" + 
+		"&client_id=303128741136-r2284o9d45b5c1lhpnhe3uvrd5df9def.apps.googleusercontent.com", http.StatusFound)
+}
+
 func main() {
 	readFile("keys.json")
+	http.HandleFunc("/v1/api/auth/google", googleHandler)
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
+
+var keys key
 
 func readFile(fileName string) {
 	file, e := ioutil.ReadFile("./" + fileName)
@@ -34,7 +50,7 @@ func readFile(fileName string) {
 		log.Println(e)
 		os.Exit(1)
 	}
-	var keys keys
+
 	e = json.Unmarshal(file, &keys)
 	log.Println(keys)
 	if e != nil {
