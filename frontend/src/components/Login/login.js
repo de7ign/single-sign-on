8a2fcf3bf0d3bac7394d87fe7ab730d39eca4231
Nihal Murmu <nhlmrm@gmail.com>
2018-07-08 18:24:30 +0530

    Add login.css

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import './login.css'
 export default class Login extends Component {
     render() {
         return (
-            <a class="button-primary button" href="http://localhost:5000/v1/api/auth/google">Login with google</a>
+            <div class="login">
+                <a class="button-primary button" href="http://localhost:5000/v1/api/auth/google">Login with google</a>
+            </div>
         )
     }
 }
