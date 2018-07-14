9158afa296837d2e959bcaa13b4aa116fb219f9c
Nihal Murmu <nhlmrm@gmail.com>
2018-07-15 01:20:59 +0530

    Add github link

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -5,6 +5,7 @@ export default class Login extends Component {
         return (
             <div class="login">
                 <a class="button-primary button" href="http://localhost:5000/v1/api/auth/google">Login with google</a>
+                <a class="button-primary button" href="http://localhost:5000/v1/api/auth/github">Login with github</a>
             </div>
         )
     }
